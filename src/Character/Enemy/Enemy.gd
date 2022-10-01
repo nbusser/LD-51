028478463dd5f1a785888a91469d662519585dcf
Nicolas Busser <nicolas.busser@lacollection.io>
2022-10-01 23:12:35 +0200

    change speed of enemy when blind

--- src/Character/Enemy/Enemy.gd
+++ src/Character/Enemy/Enemy.gd
@@ -1,10 +1,14 @@
 extends Character
 
+onready var normal_wait_time = $MoveTick.wait_time
+const SPEED_MALUS_BLIND = 2
+
+var is_blind = false
+
 func _init().("../../../"):
 	pass
 	
 func _process(delta):
-	# TODO: IA that calls move
 	if can_move():
 		var origin = global_position
 		var target = self.manager.get_player_position(true)
@@ -14,5 +18,20 @@ func _process(delta):
 			var destination = map.tilemap.world_to_map(path[0])
 			move_to(destination)
 
-func accelerate():
-	$MoveTick.wait_time /= 2
+func change_speed(new_speed):
+	$Tween.interpolate_property(
+		$MoveTick, "wait_time",
+		$MoveTick.wait_time, new_speed,
+		0.5, Tween.TRANS_LINEAR, Tween.EASE_IN
+	)
+	$Tween.start()
+
+
+func _on_EnemyBlindZone_area_entered(area):
+	is_blind = true
+	change_speed(normal_wait_time*SPEED_MALUS_BLIND)
+
+
+func _on_EnemyBlindZone_area_exited(area):
+	is_blind = false
+	change_speed(normal_wait_time)
