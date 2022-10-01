5a924660326d90fb12867af5b14d3df912b29e30
Nicolas Busser <nicolas.busser@lacollection.io>
2022-10-01 15:15:58 +0200

    add basic movement

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
-onready var map = get_parent()
-onready var game = map.get_parent()
-
+onready var map = get_parent().get_parent()
+onready var move_tick_timer = $MoveTick
 
 func _ready():
 	# Waits for Game.gd to run randomize()
 	yield(get_tree(), "idle_frame")
 	$SoundFx/SpawnSound.play_sound()
 	
-	var direction := Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")).normalized()
-	print(direction)
-#func _process(delta):
-#	pass
+	self.position -= Vector2(int(self.position.x) % 32, int(self.position.y) % 32)
+
+func can_move():
+	return move_tick_timer.is_stopped()
+
+func get_player_speed():
+	return move_tick_timer.wait_time
+
+func _process(delta):
+	var direction := Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
+	# No diagonal move
+	if(direction.x != 0 and direction.y != 0):
+		direction.y = 0
+
+
+	if direction != Vector2.ZERO and can_move():
+		move_tick_timer.start()
+		
+		var destination = map.tilemap.map_to_world(
+			map.tilemap.world_to_map(global_position) + direction
+		)
+		
+		$Tween.interpolate_property(
+			self, "position", self.position, destination, get_player_speed(), Tween.TRANS_CUBIC, Tween.EASE_IN_OUT
+		)
+		$Tween.start()
