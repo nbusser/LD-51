c2888017d62298294a593558b573997b628d27ca
Nicolas Busser <nicolas.busser@lacollection.io>
2022-10-02 13:16:55 +0200

    refactor all interactibles to new folder

--- src/Interactibles/Door/Doors.gd
+++ src/Interactibles/Door/Doors.gd
@@ -1,6 +1,6 @@
 extends Node
 
-onready var door = preload("res://src/Door/Door.tscn")
+onready var door = preload("res://src/Interactibles/Door/Door.tscn")
 onready var map = $"../../"
 onready var tilemap = $"../TileMap"
 
--- src/LightBulb/LightBulbs.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-onready var light_bulb = preload("res://src/LightBulb/LightBulb.tscn")
-
-func create_light_bulb(pos):
-	var bulb = light_bulb.instance()
-	add_child(bulb)
