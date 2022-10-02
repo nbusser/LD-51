7dca01984a7cbe4eda778442cad068c31f71d3e9
Nicolas Busser <nicolas.busser@lacollection.io>
2022-10-02 13:33:32 +0200

    fix level 2

--- src/SceneManager/SceneManager.gd
+++ src/SceneManager/SceneManager.gd
@@ -11,7 +11,7 @@ onready var music_players = $Musics.get_children()
 onready var main_menu = preload("res://src/MainMenu/MainMenu.tscn")
 onready var levels = [
 	preload("res://src/Level/Level1.tscn"),
-	#preload("res://src/Level/Level2.tscn")
+	preload("res://src/Level/Level2.tscn")
 ]
 onready var change_level = preload("res://src/EndLevel/EndLevel.tscn")
 onready var credits = preload("res://src/Credits/Credits.tscn")
